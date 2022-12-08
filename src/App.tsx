4665ae5f8e518527425b0f52144cbf3f2e1d22a4
Magdalena <magdalenahyra@wp.pl>
2022-12-08 23:12:00 +0100

    Add handleAdd function

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Header from './components/Header/Header';
 import InputField from './components/InputField/InputField';
+import {Task} from './model';
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (task) {
+      setTasks([...tasks, {id: Date.now(), task, isDone: false}]);
+      setTask('');
+    }
+  };
+
   return (
     <>
       <Header />
-      <InputField />
+      <InputField task={task} setTask={setTask} handleAdd={handleAdd} />
     </>
   );
 };
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './InputField.module.scss';
 
-const InputField = () => {
+interface Props {
+  task: string;
+  setTask: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({task, setTask, handleAdd}) => {
   return (
     <div className={styles.input_wrapper}>
-      <form className={styles.input_container}>
+      <form className={styles.input_container} onSubmit={handleAdd}>
         <input
           type='input'
           placeholder='Enter new task'
           className={styles.input}
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
         />
         <button type='submit' className={styles.input_submit_btn}>
           Add
