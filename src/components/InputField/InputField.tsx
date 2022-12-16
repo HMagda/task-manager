9b124ad08b7046f59815e0bf8615e051199c1886
Magdalena <magdalenahyra@wp.pl>
2022-12-16 17:59:56 +0100

    Change new task and tasks textareas to inputs, and add Delete and Edit task handlers

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,6 +1,5 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import styles from './InputField.module.scss';
-import useAutosizeTextArea from '../../useAutosizeTextArea';
 
 interface Props {
   task: string;
@@ -9,19 +8,13 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({task, setTask, handleAdd}) => {
-  const textAreaRef = useRef<HTMLTextAreaElement>(null);
-
-  useAutosizeTextArea(textAreaRef.current, task);
-
   return (
     <div className={styles.input_wrapper}>
       <form className={styles.input_container} onSubmit={handleAdd}>
-        <textarea
+        <input
           placeholder='Enter new task'
           className={styles.input}
           value={task}
-          ref={textAreaRef}
-          rows={1}
           onChange={(e) => setTask(e.target.value)}
         />
         <button type='submit' className={styles.input_submit_btn}>
--- src/components/SingleTask/SingleTask.tsx
+++ src/components/SingleTask/SingleTask.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import styles from './SingleTask.module.scss';
 import {Task} from '../../model';
 import {FiEdit, FiTrash2, FiCheckCircle} from 'react-icons/fi';
@@ -10,17 +10,62 @@ interface Props {
 }
 
 const SingleTask: React.FC<Props> = ({task, tasks, setTasks}) => {
+  const [edited, setEdited] = useState<boolean>(false);
+  const [editedTask, setEditedTask] = useState<string>(task.task);
+
+  const handleDelete = (id: number) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    console.log('dziala');
+    e.preventDefault();
+
+    setTasks(
+      tasks.map((task) => (task.id === id ? {...task, task: editedTask} : task))
+    );
+    setEdited(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edited]);
+
   return (
-    <form className={styles.single_task_container}>
-      <span className={styles.single_task_text}>{task.task}</span>
+    <form
+      className={styles.single_task_container}
+      onSubmit={(e) => handleEdit(e, task.id)}
+    >
+      {edited ? (
+        <input
+          className={`${styles.input} ${styles.single_task_text}`}
+          value={editedTask}
+          onChange={(e) => setEditedTask(e.target.value)}
+          ref={inputRef}
+        />
+      ) : (
+        <div className={styles.single_task_text}>{task.task}</div>
+      )}
+
       <div className={styles.icon_container}>
-        <span className={styles.icon}>
+        <span
+          className={styles.icon}
+          onClick={() => {
+            if (!edited) {
+              setEdited(!edited);
+            }
+          }}
+        >
           <FiEdit />
         </span>
         <span className={styles.icon}>
           <FiCheckCircle />
         </span>
-        <span className={`${styles.icon} ${styles.trash}`}>
+        <span
+          className={`${styles.icon} ${styles.trash}`}
+          onClick={() => handleDelete(task.id)}
+        >
           <FiTrash2 />
         </span>
       </div>
